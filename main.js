b1b7ae502f6fc67c7e629b5d019766a0a38620bf
Michael al Houwayek <156347349+michaelalhouwayek@users.noreply.github.com>
2024-01-27 17:49:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,115 +1,46 @@
 
-// 1 is snowy
-//2 is stormy
-//3 is rainy
-//4 is cloudy
-//5 is sunny
-meteo = randint(1,5)
-
-var temperature
-
-if (meteo === 1) {
-  temperature == randint(15,20)
-  $("#neige").show()
-}
-if (meteo === 2) {
-  temperature == randint(17,22)
-  $("#tempete").show()
-}
-if (meteo === 3) {
-  temperature == randint (19,24)
-  $("#pluie").show()
-}
-if (meteo === 4) {
-  temperature == randint (22,27)
-  $("#nuage").show()
-}
-if (meteo === 5) {
-  temperature == randint (27,35)
-  $("#soleil").show()
-}
-
-
 function randint(min, max) {
   var alea = min + Math.floor(Math.random() * (max - min + 1));
   //console.log(alea);
   return alea;
 }
 
-var intervalle_de_creation = randint(750, 3000);
+meteo = randint(1,5) // 1 is snowy ; 2 is stormy ; 3 is rainy ; 4 is cloudy ; 5 is sunny
+previousMeteo = "";
+temperature = null;
 
-function creerCaseElements() {
-  var cellule = $(
-    "table tr:nth-of-type(" +
-      randint(1, 10) +
-      ") td:nth-of-type(" +
-      randint(1, 20) +
-      ")"
-  ); //.css("background","gray")
+function setMeteo(meteo) {
+  $(previousMeteo).hide();
 
-  if (cellule.html() == "") {
-    var cloneP = $("#PereNoel").clone();
-    cloneP.removeAttr("id");
-    cloneP.appendTo(cellule);
-
-    setTimeout(function () {
-      cloneP.remove();
-    }, intervalle_de_creation);
+  if (meteo === 1) {
+    temperature = randint(15,20)
+    $("#neige").show() ; previousMeteo = "#neige";
+  }
+  else if (meteo === 2) {
+    temperature = randint(17,22)
+    $("#tempete").show() ; previousMeteo = "#tempete";
+  }
+  else if (meteo === 3) {
+    temperature = randint(19,24)
+    $("#pluie").show() ; previousMeteo = "#pluie";
+  }
+  else if (meteo === 4) {
+    temperature = randint(22,27)
+    $("#nuage").show() ; previousMeteo = "#nuage";
+  }
+  else if (meteo === 5) {
+    temperature = randint(27,35)
+    $("#soleil").show() ; previousMeteo = "#soleil";
   }
 }
 
-/**
- * Fonction lancerJeu
- * Lancement du jeu : crée des personnages à intervalle irrégulier
- */
 function lancerJeu() {
-  $("#boutton").attr("disabled", "disabled");
-  tempsDeJeu = 45;
-  tpsEle = $("#Temps");
-  tpsEle.text(tempsDeJeu);
-  var tps = parseInt(tpsEle.text());
-
-  boucle1 = setInterval(function () {
-    tps -= 1;
-    tpsEle.text(tps);
-
-    if (tps == 0) {
-      clearInterval(boucle1);
-      clearInterval(boucle2);
-      console.log(
-        "Le jeux est terminé! En " +
-          tps +
-          " secondes vous avez eu un score de " +
-          $("#Score").html() +
-          ""
-      );
-    }
-  }, 1000);
-
-  boucle2 = setInterval(function () {
-    alea = randint(1, 2);
-    if (alea == 1) {
-      creerPereNoel();
-    } else {
-      creerGrinch();
-    }
-  }, randint(500, 2000));
+  $("#startGame").attr("disabled", "disabled");
+  setMeteo(meteo);
 }
 
-/**
- * Fonction augmenterScore
- * Incrémente le score de 1
- */
-function augmenterScore() {
-  score = $("#Score");
-  valScore = parseInt(score.text());
-  val = 1 + valScore;
-  score.text(val);
-}
-
-function diminuerScore() {
-  score = $("#Score");
-  valScore = parseInt(score.text());
-  val = valScore - 5;
-  score.text(val);
-}
+/*
+function changerArgent(amount) {
+  argent = $("#argent");
+  argent.text() change etc
+} */
