83c9aecd0c970337ba7c507cbaab0d720d7240ee
Michael al Houwayek <156347349+michaelalhouwayek@users.noreply.github.com>
2024-03-04 18:11:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -348,3 +348,135 @@ function changeSliderVal(slider,price) {
     slicePrice = val
   }
 }
+
+// walkFuncs -- walking out function wouldnt work either bc more than one func?
+function c1Walk() {
+  customer = cust1 ; customer.show()
+  standDest = parseFloat(standHTML.css("margin-left"))+15
+  currentPos = parseFloat(customer.css("margin-left"))
+  endDest =  window.innerWidth
+
+  var walkingToLemonade = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      
+      if (customer.css("margin-left").match(/\d+/g)[0] >= standDest) {
+      customerPromptPurchase() ; walkingOut1() ; clearInterval(walkingToLemonade)
+      }
+  }, 50);
+
+  function walkingOut1() {
+      console.log("Started")
+      wo = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      if (customer.css("margin-left").match(/\d+/g)[0] >= endDest) {
+          c1Live = false ; customer.hide() ; clearInterval(wo)
+      }
+      }, 50);
+  }
+}
+function c2Walk() {
+  customer = cust2 ; customer.show()
+  standDest = parseFloat(standHTML.css("margin-left"))+15
+  currentPos = parseFloat(customer.css("margin-left"))
+  endDest =  window.innerWidth
+
+  var walkingToLemonade = setInterval(function() {
+    currentPos += 5
+    customer.css("margin-left",currentPos+"px")
+    
+    if (customer.css("margin-left").match(/\d+/g)[0] >= standDest) {
+      customerPromptPurchase() ; walkingOut() ; clearInterval(walkingToLemonade)
+    }
+  }, 50);
+  
+  function walkingOut() {
+    console.log("Started")
+    wo = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      if (customer.css("margin-left").match(/\d+/g)[0] >= endDest) {
+        c2Live = false ; customer.hide() ; clearInterval(wo)
+      }
+    }, 50);
+  }
+}
+function c3Walk() {
+  customer = cust3 ; customer.show()
+  standDest = parseFloat(standHTML.css("margin-left"))+15
+  currentPos = parseFloat(customer.css("margin-left"))
+  endDest =  window.innerWidth
+
+  var walkingToLemonade = setInterval(function() {
+    currentPos += 5
+    customer.css("margin-left",currentPos+"px")
+    
+    if (customer.css("margin-left").match(/\d+/g)[0] >= standDest) {
+      customerPromptPurchase() ; walkingOut() ; clearInterval(walkingToLemonade)
+    }
+  }, 50);
+  
+  function walkingOut() {
+    console.log("Started")
+    wo = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      if (customer.css("margin-left").match(/\d+/g)[0] >= endDest) {
+        c3Live = false ; customer.hide() ; clearInterval(wo)
+      }
+    }, 50);
+  }
+}
+function c4Walk() {
+  customer = cust4 ; customer.show()
+  standDest = parseFloat(standHTML.css("margin-left"))+15
+  currentPos = parseFloat(customer.css("margin-left"))
+  endDest =  window.innerWidth
+
+  var walkingToLemonade = setInterval(function() {
+    currentPos += 5
+    customer.css("margin-left",currentPos+"px")
+    
+    if (customer.css("margin-left").match(/\d+/g)[0] >= standDest) {
+      customerPromptPurchase() ; walkingOut() ; clearInterval(walkingToLemonade)
+    }
+  }, 50);
+  
+  function walkingOut() {
+    console.log("Started")
+    wo = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      if (customer.css("margin-left").match(/\d+/g)[0] >= endDest) {
+        c4Live = false ; customer.hide() ; clearInterval(wo)
+      }
+    }, 50);
+  }
+}
+function c5Walk() {
+  customer = cust5 ; customer.show()
+  standDest = parseFloat(standHTML.css("margin-left"))+15
+  currentPos = parseFloat(customer.css("margin-left"))
+  endDest =  window.innerWidth
+
+  var walkingToLemonade = setInterval(function() {
+    currentPos += 5
+    customer.css("margin-left",currentPos+"px")
+    
+    if (customer.css("margin-left").match(/\d+/g)[0] >= standDest) {
+      customerPromptPurchase() ; walkingOut() ; clearInterval(walkingToLemonade)
+    }
+  }, 50);
+  
+  function walkingOut() {
+    console.log("Started")
+    wo = setInterval(function() {
+      currentPos += 5
+      customer.css("margin-left",currentPos+"px")
+      if (customer.css("margin-left").match(/\d+/g)[0] >= endDest) {
+        c5Live = false ; customer.hide() ; clearInterval(wo)
+      }
+    }, 50);
+  }
+}
